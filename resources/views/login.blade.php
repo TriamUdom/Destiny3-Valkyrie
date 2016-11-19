85ffa0bd8988157d0ca6260e1f00c39ced4f5fc1
theminer3746 <bwsminecraft@hotmail.com>
2016-11-19 15:28:52 +0700

    Login blade now extend from master

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,6 +1,10 @@
+@extends('layouts.master')
+
+@section('content')
 <form action="/login" method="post">
     <input type="text" name="username" placeholder="Username"><br />
     <input type="password" name="password" placeholder="Password"><br />
     {{ csrf_field() }}
     <input type="submit" value="Login">
 </form>
+@endsection
