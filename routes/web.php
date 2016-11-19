4045b44c25c564b51234ac4bd5a7ed77855bce24
theminer3746 <bwsminecraft@hotmail.com>
2016-11-19 11:52:46 +0700

    Add a bunch of routes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,13 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/login', 'AdminController@showLoginPage');
+Route::post('/login', 'AdminController@login');
+
+// Authorization required beyond this point
+Route::group(['middleware' => 'admin'], function(){
+    Route::get('/', 'AdminController@showIndexPage');
+
+    Route::get('/logout', 'AdminController@showLogoutPage');
+    Route::post('/logout', 'AdminController@logout');
 });
