9a740d809c3057a03bf05634be483e02322a7bf5
theminer3746 <bwsminecraft@hotmail.com>
2016-11-19 11:57:38 +0700

    Add method to show index page

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller{
+    public function showIndexPage(){
+        return view('master');
+    }
+
     public static function adminLoggedIn(): bool {
         return (session('applicant_logged_in') == 1);
     }
