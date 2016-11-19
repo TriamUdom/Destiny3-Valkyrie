5459bc92741eca820bba55ecea192d596a96cbe9
theminer3746 <bwsminecraft@hotmail.com>
2016-11-19 14:40:46 +0700

    Change model

--- app/Admin.php
+++ app/Admin.php
@@ -2,9 +2,10 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+use Jenssegers\Mongodb\Eloquent\Model;
 
 class Admin extends Model
 {
-    //
+    protected $table = 'admins';
+    public $timestamps = false;
 }
