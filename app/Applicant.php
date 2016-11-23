37284f10653ac776847d4a8999457c55d3611753
theminer3746 <bwsminecraft@hotmail.com>
2016-11-23 10:40:14 +0700

    Create Applicant model

--- app/Applicant.php
+++ app/Applicant.php
@@ -2,9 +2,44 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+use Jenssegers\Mongodb\Eloquent\Model;
 
 class Applicant extends Model
 {
-    //
+    protected $table = 'applicants';
+    protected $fillable = [
+        'citizen_id',
+        'title',
+        'fname',
+        'lname',
+        'title_en',
+        'fname_en',
+        'lname_en',
+        'gender',
+        'email',
+        'phone',
+        'birthdate',
+        'staying_with_parent',
+        'father',
+        'mother',
+        'guardian',
+        'school',
+        'graduation_year',
+        'gpa',
+        'school_move_in',
+        'school_province',
+        'address',
+        'application_type',
+        'quota_type',
+        'plan',
+        'majors',
+        'quota_grade',
+        'documents',
+        'check_status',
+    ];
+
+    public $timestamps = false;
+    // Timestamp has to be disable because it'll throw
+    // MongoDB\Driver\Exception\InvalidArgumentException with message
+    // 'MongoDB\BSON\UTCDateTime::__construct() expects parameter 1 to be integer, float given'
 }
