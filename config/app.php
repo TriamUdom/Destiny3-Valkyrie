ad1d6194450d978b7c2f3359f34d80819d238535
theminer3746 <bwsminecraft@hotmail.com>
2016-11-23 10:41:46 +0700

    Add RESTResponse to facade list

--- config/app.php
+++ config/app.php
@@ -228,7 +228,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'RESTResponse' => App\Facades\RESTResponse::class,
     ],
 
 ];
