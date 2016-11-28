ef1e21c426cc124aee3b633ef12294e214685fa4
theminer3746 <bwsminecraft@hotmail.com>
2016-11-28 13:46:56 +0700

    Demo how to use verifyGrade

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -4,6 +4,8 @@
     @if(empty($data))
         ไม่มีข้อมูล
     @else
-        
+        {{ var_dump(App\Requirement::verifyGrade($data['plan'], $data['quota_grade'], $data['gpa'])) }}
+
+
     @endif
 @endsection
