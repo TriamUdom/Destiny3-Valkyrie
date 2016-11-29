7331e8d98cabe2aa276b34556a903a8a28eb0a0b
theminer3746 <bwsminecraft@hotmail.com>
2016-11-29 16:53:14 +0700

    Proof of concept for accept/decline form

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -6,6 +6,10 @@
     @else
         {{ var_dump(App\Requirement::verifyGrade($data['plan'], $data['quota_grade'], $data['gpa'])) }}
 
-
+        <form action="/applicants/status/{{ $data['_id'] }}" method="post">
+            <input type="hidden" value="1" name="status">
+            <input type="submit" value="accept">
+            {{ csrf_field() }}
+        </form>
     @endif
 @endsection
