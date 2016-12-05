f0ece438ada53bd66050a95581bf9969289df6dd
theminer3746 <bwsminecraft@hotmail.com>
2016-12-05 16:55:17 +0700

    Add methods about supervisor

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,10 +6,45 @@ use Illuminate\Http\Request;
 use App\Admin as Admin;
 use Hash;
 use Session;
+use App\Supervisor as Supervisor;
 
 class AdminController extends Controller{
     public function showIndexPage(){
         return view('index');
+    }
+
+    public function showSupervisorLoginPage(){
+        return view('supervisor_login');
+    }
+
+    public function supervisorLogin(Request $request){
+        $this->validate($request, [
+            'username' => 'required',
+            'password' => 'required',
+        ]);
+
+        $db = Supervisor::where('username', $request->input('username'))->first();
+
+        if(count($db) === 1){
+            if(Hash::check($request->input('password'), $db->password)){
+                Session::put('supervisor_logged_in', 1);
+                Session::put('supervisor_name', $db->name);
+
+                return redirect('/login');
+            }else{
+                //TODO handle error properly
+                dd('incorrect password');
+                return false;
+            }
+        }else{
+            //TODO handle error properly
+            dd('user not found');
+            return false;
+        }
+    }
+
+    public static function supervisorLoggedIn(): bool {
+        return (session('supervisor_logged_in') == 1);
     }
 
     public function showLoginPage(){
