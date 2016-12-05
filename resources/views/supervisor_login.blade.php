1bf10abb38ce8ddcd7ec3dc0dd7515b1ed5e5083
Namo <namo@namodev.com>
2016-12-05 22:40:31 +0700

    Added visual cues for user/supervisor login pages

--- resources/views/supervisor_login.blade.php
+++ resources/views/supervisor_login.blade.php
@@ -7,12 +7,12 @@
     <div class="col-md-8 col-md-offset-2">
         <div class="well">
             <form action="/supervisor_login" method="post">
-                <legend><i class="fa fa-lock"></i> <b>กรุณาเข้าสู่ระบบก่อน</b></legend>
+                <legend><i class="fa fa-lock"></i> <b>กรุณาเข้าสู่ระบบก่อน (1/2)</b> <i class="pull-right text-muted">Supervisor Account</i></legend>
                 <input type="text" name="username" placeholder="ชื่อผู้ใช้" class="form-control">
                 <div style="height:10px;"></div>
                 <input type="password" name="password" placeholder="รหัสผ่าน" class="form-control"><br />
                 {{ csrf_field() }}
-                <button type="submit" class="btn btn-block btn-primary">เข้าสู่ระบบ</button>
+                <button type="submit" class="btn btn-block btn-info">เข้าสู่ระบบ</button>
             </form>
         </div>
     </div>
