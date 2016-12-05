1ac96314ddd337949fe818f5ec4004322989e69b
theminer3746 <bwsminecraft@hotmail.com>
2016-12-05 16:55:30 +0700

    Create supervisor login page

---
+++ resources/views/supervisor_login.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.master')
+@section('title', 'Valkyrie Login')
+
+@section('content')
+
+<div class="row">
+    <div class="col-md-8 col-md-offset-2">
+        <div class="well">
+            <form action="/supervisor_login" method="post">
+                <legend><i class="fa fa-lock"></i> <b>กรุณาเข้าสู่ระบบก่อน</b></legend>
+                <input type="text" name="username" placeholder="ชื่อผู้ใช้" class="form-control">
+                <div style="height:10px;"></div>
+                <input type="password" name="password" placeholder="รหัสผ่าน" class="form-control"><br />
+                {{ csrf_field() }}
+                <button type="submit" class="btn btn-block btn-primary">เข้าสู่ระบบ</button>
+            </form>
+        </div>
+    </div>
+</div>
+
+
+@endsection
