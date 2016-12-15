fa25f02eac907e1058c009a88b77392ed2e28014
theminer3746 <bwsminecraft@hotmail.com>
2016-12-15 23:02:30 +0700

    Add support for english title

--- app/UIHelper.php
+++ app/UIHelper.php
@@ -5,24 +5,41 @@ namespace App;
 use Exception;
 
 class UIHelper{
-    public static function formatTitle($title){
+    public static function formatTitle($title, $langague = 'tha'){
         if(is_numeric($title)){
             switch($title){
               // 0,2 is male - 1,3 is female - 4 shouldn't really be defined:
-              case 0:
-                return 'ด.ช.';
-              break;
-              case 1:
-                return 'ด.ญ.';
-              break;
-              case 2:
-                return 'นาย';
-              break;
-              case 3:
-                return 'นางสาว';
-              break;
-              default:
-                throw new \Exception("What the gender?");
+                case 0:
+                    if($langague == 'eng'){
+                        return 'Mr.';
+                    }
+
+                    return 'ด.ช.';
+                break;
+                case 1:
+                    if($langague == 'eng'){
+                        return 'Ms.';
+                    }
+
+                    return 'ด.ญ.';
+                break;
+                case 2:
+                    if($langague == 'eng'){
+                        return 'Mr.';
+                    }
+
+                    return 'นาย';
+                break;
+                case 3:
+                    if($langague == 'eng'){
+                        return 'Ms.';
+                    }
+
+                    return 'นางสาว';
+                break;
+                default:
+                    throw new \Exception("What the gender?");
+                break;
             }
         }else{
             return $title;
