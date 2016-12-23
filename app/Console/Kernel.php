01b0db5b224cfcc17ececf7ee284dbdf00b86522
theminer3746 <bwsminecraft@hotmail.com>
2016-12-24 02:23:33 +0700

    Register report excel command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         Commands\NotifyUI::class,
+        Commands\ReportApplicant::class,
         //Commands\FormatData::class,
     ];
 
